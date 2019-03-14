14dd610993ff45aae5fb1ee577a12e6295339548
romfol <romanfoltyn1@gmail.com>
2019-03-14 09:45:03 +0200

    better pagination logic

--- src/components/list/list.jsx
+++ src/components/list/list.jsx
@@ -16,67 +16,62 @@ class List extends Component {
 
     return tasks.length ? (
       <ul className="List">
-        {tasks.map((task, i) => {
-          while (i >= indexFirstTask && i <= indexLastTask) {
-            if (task.onEdit) {
-              return (
-                <li className="tasks" key={task.timeId}>
-                  <input
-                    type="checkbox"
-                    checked={task.isChecked}
-                    onChange={e => this.props.markChecked(task.timeId, e)}
-                  />
-                  <input
-                    type="text"
-                    placeholder="New task..."
-                    value={this.state.value}
-                    onChange={this.handleChange}
-                  />
-                  <input
-                    type="checkbox"
-                    checked={task.isDone}
-                    onChange={e => this.props.markTask(task.timeId, e)}
-                  />
-                  <button
-                    className="Edit-button"
-                    onClick={() => this.props.submitChangeTask(this.state.value, task.timeId)}
-                  >
-                    Save
-                  </button>
-                  <button className="Delete-button" onClick={() => this.props.onEdit(task.timeId)}>
-                    Cancel
-                  </button>
-                </li>
-              );
-            } else
-              return (
-                <li className="tasks" key={task.timeId}>
-                  <input
-                    type="checkbox"
-                    checked={task.isChecked}
-                    onChange={e => this.props.markChecked(task.timeId, e)}
-                  />
-                  <span className={task.isDone ? 'marked-title' : 'regular-title'}>
-                    {task.task}
-                  </span>
-                  <input
-                    type="checkbox"
-                    checked={task.isDone}
-                    onChange={e => this.props.markTask(task.timeId, e)}
-                  />
-                  <button className="Edit-button" onClick={() => this.props.onEdit(task.timeId)}>
-                    Edit
-                  </button>
-                  <button
-                    className="Delete-button"
-                    onClick={() => this.props.removeTask(task.timeId)}
-                  >
-                    Remove
-                  </button>
-                </li>
-              );
-          }
-          return null;
+        {tasks.slice(indexFirstTask, indexLastTask + 1).map(task => {
+          if (task.onEdit) {
+            return (
+              <li className="tasks" key={task.timeId}>
+                <input
+                  type="checkbox"
+                  checked={task.isChecked}
+                  onChange={e => this.props.markChecked(task.timeId, e)}
+                />
+                <input
+                  type="text"
+                  placeholder="New task..."
+                  value={this.state.value}
+                  onChange={this.handleChange}
+                />
+                <input
+                  type="checkbox"
+                  checked={task.isDone}
+                  onChange={e => this.props.markTask(task.timeId, e)}
+                />
+                <button
+                  className="Edit-button"
+                  onClick={() => this.props.submitChangeTask(this.state.value, task.timeId)}
+                >
+                  Save
+                </button>
+                <button className="Delete-button" onClick={() => this.props.onEdit(task.timeId)}>
+                  Cancel
+                </button>
+              </li>
+            );
+          } else
+            return (
+              <li className="tasks" key={task.timeId}>
+                <input
+                  type="checkbox"
+                  checked={task.isChecked}
+                  onChange={e => this.props.markChecked(task.timeId, e)}
+                />
+                <span className={task.isDone ? 'marked-title' : 'regular-title'}>{task.task}</span>
+                <input
+                  type="checkbox"
+                  checked={task.isDone}
+                  onChange={e => this.props.markTask(task.timeId, e)}
+                />
+                <button className="Edit-button" onClick={() => this.props.onEdit(task.timeId)}>
+                  Edit
+                </button>
+                <button
+                  className="Delete-button"
+                  onClick={() => this.props.removeTask(task.timeId)}
+                >
+                  Remove
+                </button>
+              </li>
+            );
         })}
       </ul>
     ) : (
